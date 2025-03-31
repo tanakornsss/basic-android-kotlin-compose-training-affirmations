9a5ea96fd9bcd5ec085e29ba4896b78d0fd14fad
tanakornsss <omtanakorn2550@gmail.com>
2025-03-31 21:26:07 +0700

    Finished the app.

--- app/src/main/java/com/example/affirmations/ui/theme/Theme.kt
+++ app/src/main/java/com/example/affirmations/ui/theme/Theme.kt
@@ -62,7 +62,7 @@ fun AffirmationsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
+            // window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
